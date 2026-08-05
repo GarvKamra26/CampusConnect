ded139724c9c546c83979e44ff32f531ef974359
GarvKamra26 <garvkamra1091@gmail.com>
2026-08-05 15:48:10 +0530

    setup vite app

--- backend/server.js
+++ backend/server.js
@@ -14,18 +14,6 @@ app.get('/', (req,res)=>{
     res.send("Welcome to campusConnect")
 })
 
-async function testDatabase() {
-    try {
-        const connection = await db.getConnection();
-        console.log("✅ Connected to MySQL!");
-        connection.release();
-    } catch (err) {
-        console.error("❌ Database connection failed:", err);
-    }
-}
-
-testDatabase();
-
 app.listen(3000, ()=> {
     console.log("Server running...")
 })
